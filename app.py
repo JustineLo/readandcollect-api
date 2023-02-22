94d626d360416fe7c472dd4e8c1beeb138cc0a79
Justine Lo <juustine.lo@gmail.com>
2023-02-22 07:28:38 +0100

    refacto

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import json
 
-from utils import get_html, sanitize, select_content
+from bs4 import BeautifulSoup, Comment
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -34,22 +35,12 @@ def add_article():
     }
     html = get_html(url, headers)
 
-    title = ""
-    try: 
-        if(html.h1):
-            title = html.h1.text
-        elif(html.title):
-            title = html.title.text
-        else:
-            title = "Untitled"
-    except:
-        print("title catch error")
+    title = get_title(html)
 
     html = sanitize(html.article if html.article else html.body)
     html = select_content(html)
-    text = ""
-    for tag in html:
-        text += str(tag)
+
+    text = get_text(html)
 
     # define the new user data
     new_article = {
@@ -67,3 +58,69 @@ def add_article():
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
+def get_html(url, headers):
+    request = requests.get(url, headers=headers)
+    html = BeautifulSoup(request.content, 'html.parser')
+    return html
+
+def get_title(html):
+    title = ""
+    try: 
+        if(html.h1):
+            title = html.h1.text
+        elif(html.title):
+            title = html.title.text
+        else:
+            title = "Untitled"
+    except:
+        print("title catch error")
+    return title
+
+def get_text(html):
+    text = ""
+    for tag in html:
+        text += str(tag)
+    return text
+
+
+def sanitize(soup):
+    tag_blacklist = ['script', 'style', 'img',
+                     'object', 'embed', 'iframe', 'svg', 'header', 'footer', 'aside', 'button', 'nav', 'audio', 'video', 'input', 'form', 'textarea']
+
+    # Remove HTML comments
+    for comment in soup.findAll(
+            text=lambda text: isinstance(text, Comment)):
+        comment.extract()
+    # remove a tags keeping content
+    for a in soup.find_all('a', href=True):
+        a.replaceWithChildren()
+    for img in soup.find_all('img'):
+        img.decompose()
+    for svg in soup.find_all('svg'):
+        svg.decompose()
+    # Remove unwanted tags
+    for tag in soup.findAll():
+        # Remove blacklisted tags and their contents.
+        if tag.name in tag_blacklist:
+            tag.decompose()
+            break
+
+    return soup
+
+
+def select_content(soup):
+    tags_to_keep = ['h1', 'h2', 'h3', 'h4',
+                    'h5', 'h6', 'ul', 'ol', 'br']
+    tag_array = list()
+    for tag in soup.find_all():
+        if tag.name == 'pre':
+            tag_array.append(tag)
+        if tag.name in tags_to_keep:
+            sanitize(tag)
+            tag_array.append(tag)
+        if tag.name == 'p':
+            sanitize(tag)
+            tag_array.append(tag)
+    return tag_array
